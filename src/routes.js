96c5657287eb83f7ccfe8e81d14efdd9011c0fb7
kaedeek <yukise30@icloud.com>
2025-10-27 18:27:19 +0900

    Updates

--- src/routes.js
+++
@@ -1,9 +0,0 @@
-import Home from "./routes/Home.svelte";
-import Docs from "./routes/Docs.svelte";
-import About from "./routes/About.svelte";
-
-export const routes = {
-  "/": Home,
-  "/docs": Docs,
-  "/about": About
-};
--- src/routes/Home.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  import CodeBackground from "../lib/CodeBackground.svelte";
-  import HeroSection from "../lib/HeroSection.svelte";
-</script>
-
-<div class="relative min-h-screen flex flex-col items-center justify-center text-center text-white bg-black overflow-hidden">
-  <CodeBackground />
-  <HeroSection />
-</div>
