794c09fe43d60c6b5c30605b9ed150d1b166b990
kaedeek <yukise30@icloud.com>
2026-01-10 00:21:57 +0900

    feat: change discord server invited link.

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -10,7 +10,7 @@
     { name: "GitHub", url: "https://github.com/VersaLog", icon: "github" },
     { name: "X", url: "https://x.com/logversa81950", icon: "twitter" },
     { name: "Qiita", url: "https://qiita.com/organizations/versatile", icon: "code" },
-    { name: "Support", url: "https://discord.gg/eQjWM7MT", icon: "message-circle" }
+    { name: "Support", url: "https://discord.gg/bEUneaBg", icon: "message-circle" }
   ];
 
   function getIcon(name) {
