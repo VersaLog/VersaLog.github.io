4b95269163b42ad5b9a0926016ac2fc8203d9099
kaedeek <yukise30@icloud.com>
2026-04-22 18:17:11 +0900

    docs(Update): all Language docs

---
+++ src/routes/docs/Dart.svelte
@@ -0,0 +1,14 @@
+<script>
+  const code = `import 'package:versalog/versalog.dart';
+
+void main() {
+  final logger = VersaLog("detailed");
+  logger.info("info");
+}`;
+</script>
+
+<h1>🎯 Dart</h1>
+
+<pre class="whitespace-pre-wrap break-words text-green-300">
+<code>{code}</code>
+</pre>
